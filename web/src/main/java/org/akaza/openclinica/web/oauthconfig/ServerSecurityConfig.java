088521c028848d51a78b78b2dccc0cddde607a42
Zique Wood-Ferren <iwoodferren@openclinica.com>
2023-06-26 09:07:57 -0400

    OC-21000: removed several files

--- web/src/main/java/org/akaza/openclinica/web/oauthconfig/ServerSecurityConfig.java
+++ web/src/main/java/org/akaza/openclinica/web/oauthconfig/ServerSecurityConfig.java
@@ -28,7 +28,7 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/oauth/token").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll();
